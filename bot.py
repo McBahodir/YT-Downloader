9c1772206043fcb19761643613467adbb27778d0
McBahodir <130217682+McBahodir@users.noreply.github.com>
2023-05-18 21:30:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ import os
 from aiogram import Bot, Dispatcher, types, executor
 from pytube import YouTube
 
+TOKEN = "5469754814:AAGVX8Uq59SKTsdaWS8Q6dpD4cX9bD_ytE4"
+
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=5469754814:AAGVX8Uq59SKTsdaWS8Q6dpD4cX9bD_ytE4, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
